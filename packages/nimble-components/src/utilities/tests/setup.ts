5f36bd92f2c873a08d728a0c9c395c89186e60e7
Jesse Attas <jattasNI@users.noreply.github.com>
2022-10-19 17:13:59 -0500

    Multiple workarounds for karma-webpack build error

--- packages/nimble-components/src/utilities/tests/setup.ts
+++ packages/nimble-components/src/utilities/tests/setup.ts
@@ -1,5 +1,12 @@
-// Import all the test files to run in browser
+// https://github.com/testing-library/dom-testing-library/issues/1180
+// @ts-ignore
+// window.process = {
+//     env: {
+//         NODE_ENV: 'production'
+//     }
+// };
 
+// Import all the test files to run in browser
 function importAll(r: __WebpackModuleApi.RequireContext): void {
     r.keys().forEach(r);
 }
