5deeab7d202bef6eebfa99596940ba0b0181b99c
Jesse Attas <jattasNI@users.noreply.github.com>
2022-10-19 18:03:45 -0500

    remove commented out workaround

--- packages/nimble-components/src/utilities/tests/setup.ts
+++ packages/nimble-components/src/utilities/tests/setup.ts
@@ -1,11 +1,3 @@
-// https://github.com/testing-library/dom-testing-library/issues/1180
-// @ts-ignore
-// window.process = {
-//     env: {
-//         NODE_ENV: 'production'
-//     }
-// };
-
 // Import all the test files to run in browser
 function importAll(r: __WebpackModuleApi.RequireContext): void {
     r.keys().forEach(r);
