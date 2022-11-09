84c433798e73c1bb2f193202abda30bb10c92c36
elizaolariu <eliza.olariu@yahoo.com>
2022-11-09 14:47:04 +0200

    Added one test

--- packages/nimble-components/src/wafer-map/template.ts
+++ packages/nimble-components/src/wafer-map/template.ts
@@ -1,10 +1,5 @@
-import { html, when } from '@microsoft/fast-element';
+import { html } from '@microsoft/fast-element';
 import type { WaferMap } from '.';
 
 export const template = html<WaferMap>`
-            ${when(
-        x => x,
-        html<WaferMap>`
-        `
-    )}
 `;
